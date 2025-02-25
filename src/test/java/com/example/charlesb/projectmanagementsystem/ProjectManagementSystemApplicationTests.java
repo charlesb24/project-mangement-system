7767f852ea6cf7f447a5c2503617567159a371f6
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-02-24 19:20:21 -0600

    add initial project files

---
+++ src/test/java/com/example/charlesb/projectmanagementsystem/ProjectManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.charlesb.projectmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
