0f11a9dfcb156c92ec85a8415f88678665a63042
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-03-22 15:18:58 -0500

    add custom UserDetailsService implementation

--- src/main/java/com/example/charlesb/projectmanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/example/charlesb/projectmanagementsystem/config/SecurityConfig.java
@@ -1,30 +1,25 @@
 package com.example.charlesb.projectmanagementsystem.config;
 
+import com.example.charlesb.projectmanagementsystem.service.CustomUserDetailsService;
+import org.hibernate.annotations.Array;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-        userDetailsManager.setUsersByUsernameQuery("select user_id, password, enabled from users where user_id=?");
-
-        userDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
-
-        return userDetailsManager;
-    }
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -47,6 +42,16 @@ public class SecurityConfig {
     public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
         // allows querying of the active user in JPA statements
         return new SecurityEvaluationContextExtension();
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder(16, 32, 1, 1 << 14, 2);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
 }
---
+++ src/main/java/com/example/charlesb/projectmanagementsystem/service/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.example.charlesb.projectmanagementsystem.service;
+
+import com.example.charlesb.projectmanagementsystem.dao.UserRepository;
+import com.example.charlesb.projectmanagementsystem.entity.Role;
+import com.example.charlesb.projectmanagementsystem.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user != null) {
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUsername(),
+                    user.getPassword(),
+                    mapRolesToAuthorities(user.getRoles())
+            );
+        }
+
+        throw new UsernameNotFoundException("Invalid username or password.");
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .toList();
+    }
+
+}
