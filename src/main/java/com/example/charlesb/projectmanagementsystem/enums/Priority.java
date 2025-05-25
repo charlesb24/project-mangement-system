e505a58a23867963fd4f9a39d228ad8c3dba1764
Charles Bradley <198821355+charlesb24@users.noreply.github.com>
2025-05-25 18:04:20 -0500

    changes
    - add display values to enums
    - remove user table and replace with collection of divs

--- src/main/java/com/example/charlesb/projectmanagementsystem/enums/Priority.java
+++ src/main/java/com/example/charlesb/projectmanagementsystem/enums/Priority.java
@@ -1,10 +1,19 @@
 package com.example.charlesb.projectmanagementsystem.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Priority {
 
-    LOW,
-    MEDIUM,
-    HIGH,
-    URGENT
+    LOW("Low"),
+    MEDIUM("Medium"),
+    HIGH("High"),
+    URGENT("Urgent");
+
+    private final String displayValue;
+
+    Priority(String displayValue) {
+        this.displayValue = displayValue;
+    }
 
 }
--- src/main/java/com/example/charlesb/projectmanagementsystem/enums/Status.java
+++ src/main/java/com/example/charlesb/projectmanagementsystem/enums/Status.java
@@ -1,11 +1,20 @@
 package com.example.charlesb.projectmanagementsystem.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Status {
 
-    NOT_STARTED,
-    IN_PROGRESS,
-    ON_HOLD,
-    CANCELLED,
-    COMPLETE
+    NOT_STARTED("Not Started"),
+    IN_PROGRESS("In Progress"),
+    ON_HOLD("On Hold"),
+    CANCELLED("Cancelled"),
+    COMPLETE("Complete");
+
+    private final String displayValue;
+
+    Status(String displayValue) {
+        this.displayValue = displayValue;
+    }
 
 }
